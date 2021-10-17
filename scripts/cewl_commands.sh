ff3506c315a33d607450208e470c969d0f5e8605
Ramon Hernandez Leon <100383351@alumnos.uc3m.es>
2021-10-17 17:13:22 +0200

    Challenge completed

--- scripts/cewl_commands.sh
+++ scripts/cewl_commands.sh
@@ -6,17 +6,17 @@ OUTPUT_FILE=$1
 touch $OUTPUT_FILE
 
 # min length 6
-# do not visit other URLs
+# depth = 1, just one hop URLs
 # lowercase all words
 # write output to a file
 # conver non latin characters
-cewl -m 6  -d 0 --lowercase --convert-umlauts https://en.wikipedia.org/wiki/Corinna_zu_Sayn-Wittgenstein-Sayn >> $OUTPUT_FILE
+cewl -m 6  -d 1 --lowercase --convert-umlauts https://en.wikipedia.org/wiki/Corinna_zu_Sayn-Wittgenstein-Sayn >> $OUTPUT_FILE
 
 aux=`wc -l < $OUTPUT_FILE`
 
 echo "Word count from Wikipedia page (English version): $aux"
 
-cewl -m 6  -d 0 --lowercase --convert-umlauts https://es.wikipedia.org/wiki/Corinna_Larsen >> $OUTPUT_FILE
+cewl -m 6  -d 1 --lowercase --convert-umlauts https://es.wikipedia.org/wiki/Corinna_Larsen >> $OUTPUT_FILE
 
 echo "Word count from Wikipedia page (Spanish version): $((`wc -l < $OUTPUT_FILE` - $aux))"
 
